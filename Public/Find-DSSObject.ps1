8cab96be5ed4fbb1bd9eea04b39054b600a07038
Sean Quinlan <github@yster.org>
2018-11-07 20:41:44 +0000

    Added useraccountcontrol flags, along with the steps required to add the uac-constructed properties if required

--- Public/Find-DSSObject.ps1
+++ Public/Find-DSSObject.ps1
@@ -34,7 +34,7 @@ function Find-DSSObject {
         [Parameter(Mandatory = $false)]
         [ValidateNotNullOrEmpty()]
         [String[]]
-        $Properties,
+        $Properties = @('distinguishedname','objectclass','objectguid'),
 
         # The context to search - Domain or Forest.
         [Parameter(Mandatory = $false)]
@@ -54,6 +54,26 @@ function Find-DSSObject {
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]
         $Credential = [System.Management.Automation.PSCredential]::Empty
+    )
+
+    # Taken from: https://social.technet.microsoft.com/wiki/contents/articles/12037.active-directory-get-aduser-default-and-extended-properties.aspx
+    $UserAccountControl_Properties = @{
+        'accountnotdelegated'                   = 'NOT_DELEGATED'
+        'allowreversiblepasswordencryption'     = 'ENCRYPTED_TEXT_PWD_ALLOWED'
+        'doesnotrequirepreauth'                 = 'DONT_REQ_PREAUTH'
+        'enabled'                               = 'ACCOUNTDISABLE'
+        'homedirrequired'                       = 'HOMEDIR_REQUIRED'
+        'mnslogonaccount'                       = 'MNS_LOGON_ACCOUNT'
+        'passwordneverexpires'                  = 'DONT_EXPIRE_PASSWORD'
+        'passwordnotrequired'                   = 'PASSWD_NOTREQD'
+        'smartcardlogonrequired'                = 'SMARTCARD_REQUIRED'
+        'trustedfordelegation'                  = 'TRUSTED_FOR_DELEGATION'
+        'trustedtoauthfordelegation'            = 'TRUSTED_TO_AUTH_FOR_DELEGATION'
+        'usedeskeyonly'                         = 'USE_DES_KEY_ONLY'
+    }
+    $UserAccountControl_Computed_Properties = @(
+        'lockedout'
+        'passwordexpired'
     )
 
     $Function_Name = (Get-Variable MyInvocation -Scope 0).Value.MyCommand.Name
@@ -83,10 +103,17 @@ function Find-DSSObject {
         )
         $Directory_Searcher = New-Object -TypeName 'System.DirectoryServices.DirectorySearcher' -ArgumentList $Directory_Searcher_Arguments
 
-        if ($PSBoundParameters.ContainsKey('Properties')) {
-            Write-Verbose ('{0}|Adding Properties: {1}' -f $Function_Name,($Properties -join ','))
-            $Directory_Searcher.PropertiesToLoad.AddRange($Properties)
+        # All the UserAccountControl properties are generated via flags to the single "useraccountcontrol" LDAP property.
+        # So the 'useraccountcontrol' property is added to the search properties list if any of the relevant UserAccountControl properties are requested.
+        $Properties_To_Add = New-Object -TypeName 'System.Collections.ArrayList'
+        foreach ($Property in $Properties) {
+            [void]$Properties_To_Add.Add($Property)
+            if (($UserAccountControl_Properties.GetEnumerator().Name -contains $Property) -and ($Properties_To_Add -notcontains 'useraccountcontrol')) {
+                [void]$Properties_To_Add.Add('useraccountcontrol')
+            }
         }
+        Write-Verbose ('{0}|Adding Properties: {1}' -f $Function_Name,($Properties_To_Add -join ' '))
+        $Directory_Searcher.PropertiesToLoad.AddRange($Properties_To_Add)
         if ($PSBoundParameters.ContainsKey('SearchScope')) {
             Write-Verbose ('{0}|Adding SearchScope: {1}' -f $Function_Name,$SearchScope)
             $Directory_Searcher.SearchScope = $SearchScope
@@ -98,12 +125,11 @@ function Find-DSSObject {
             Write-Verbose ('{0}|Found {1} result(s)' -f $Function_Name,$Directory_Searcher_Results.Count)
             $Directory_Searcher_Results_To_Return = New-Object -TypeName 'System.Collections.ArrayList'
             foreach ($Directory_Searcher_Result in $Directory_Searcher_Results) {
-                $Result_Object = [ordered]@{}
+                $Result_Object = @{}
                 $Directory_Searcher_Result.Properties.GetEnumerator() | Sort-Object 'Name' | ForEach-Object {
                     $Current_Searcher_Result_Property   = $_.Name
                     $Current_Searcher_Result_Value      = $($_.Value)
                     $Current_Searcher_Result_Syntax     = Get-DSSAttributeSyntax -Name $Current_Searcher_Result_Property
-
                     Write-Verbose ('{0}|Property={1} Syntax={2} Value={3}' -f $Function_Name,$Current_Searcher_Result_Property,$Current_Searcher_Result_Syntax,$Current_Searcher_Result_Value)
 
                     # Reformat certain attribute types
@@ -121,10 +147,52 @@ function Find-DSSObject {
                         }
                     }
 
-                    $Result_Object[$Current_Searcher_Result_Property] = $Current_Searcher_Result_Value
+                    # Add additional constructed properties from the UserAccountControl property.
+                    if ($Current_Searcher_Result_Property -eq 'useraccountcontrol') {
+                        Write-Verbose ('{0}|UserAccountControl property found, possibly adding constructed properties...' -f $Function_Name)
+                        # Only output the 'useraccountcontrol' property if it is explicitly asked for.
+                        if (($Properties -contains '*') -or ($Properties -contains 'useraccountcontrol')) {
+                            Write-Verbose ('{0}|UserAccountControl property specified directly' -f $Function_Name)
+                            $Result_Object[$Current_Searcher_Result_Property] = $Current_Searcher_Result_Value
+                        }
+
+                        # This does the following:
+                        # - Looks through the 'useraccountcontrol' integer and extracts the flag(s) that this integer refers to.
+                        # - Loops through all the properties specified to the function and if there is a match, it will do this:
+                        #   - 1. Set a default bool value of $true if the property is named "enabled" and $false for everything else.
+                        #   - 2. If the flag is set, then it will flip the bool value to the opposite.
+                        $UserAccountControl_Attributes = [Enum]::Parse('userAccountControlFlags', $Current_Searcher_Result_Value)
+                        Write-Verbose ('{0}|UAC: Attributes currently set: {1}' -f $Function_Name,($UserAccountControl_Attributes -join ' '))
+                        $UserAccountControl_Properties.GetEnumerator() | ForEach-Object {
+                            $UserAccountControl_Property_Name = $_.Name
+                            $UserAccountControl_Property_Value = $_.Value
+                            Write-Verbose ('{0}|UAC: Checking UAC property: {1}={2}' -f $Function_Name,$UserAccountControl_Property_Name,$UserAccountControl_Property_Value)
+                            if (($Properties -contains '*') -or ($Properties -contains $UserAccountControl_Property_Name)) {
+                                Write-Verbose ('{0}|UAC: Processing Property: {1}' -f $Function_Name,$UserAccountControl_Property_Name)
+                                if ($UserAccountControl_Property_Name -eq 'enabled') {
+                                    $UserAccountControl_Property_Return = $true
+                                } else {
+                                    $UserAccountControl_Property_Return = $false
+                                }
+                                Write-Verbose ('{0}|UAC: Default value for Property "{1}" is: {2}' -f $Function_Name,$UserAccountControl_Property_Name,$UserAccountControl_Property_Return)
+                                if ($UserAccountControl_Attributes -match $UserAccountControl_Property_Value) {
+                                    $UserAccountControl_Property_Return = -not $UserAccountControl_Property_Return
+                                }
+                                Write-Verbose ('{0}|UAC: Return value for Property "{1}" is: {2}' -f $Function_Name,$UserAccountControl_Property_Name,$UserAccountControl_Property_Return)
+                                $Result_Object[$UserAccountControl_Property_Name] = $UserAccountControl_Property_Return
+                            }
+                        }
+                    } else {
+                        $Result_Object[$Current_Searcher_Result_Property] = $Current_Searcher_Result_Value
+                    }
                 }
 
-                $Directory_Searcher_Result_Object = New-Object -TypeName 'System.Management.Automation.PSObject' -Property $Result_Object
+                # Sort results and then add to a new hashtable, as PSObject requires a hashtable as Property. GetEnumerator() piped into Sort-Object changes the output to an array.
+                $Result_Object_Sorted = [ordered]@{}
+                $Result_Object.GetEnumerator() | Sort-Object -Property 'Name' | ForEach-Object {
+                    $Result_Object_Sorted[$_.Name] = $_.Value
+                }
+                $Directory_Searcher_Result_Object = New-Object -TypeName 'System.Management.Automation.PSObject' -Property $Result_Object_Sorted
                 [void]$Directory_Searcher_Results_To_Return.Add($Directory_Searcher_Result_Object)
             }
             # Return the search results object
@@ -138,3 +206,34 @@ function Find-DSSObject {
         $PSCmdlet.ThrowTerminatingError($_)
     }
 }
+
+# Taken from:
+# - https://github.com/zloeber/PSAD
+# - https://support.microsoft.com/en-us/help/305144/how-to-use-the-useraccountcontrol-flags-to-manipulate-user-account-pro
+Add-Type -TypeDefinition @"
+    [System.Flags]
+    public enum userAccountControlFlags {
+        SCRIPT                          = 0x0000001,
+        ACCOUNTDISABLE                  = 0x0000002,
+        HOMEDIR_REQUIRED                = 0x0000008,
+        LOCKOUT                         = 0x0000010,
+        PASSWD_NOTREQD                  = 0x0000020,
+        PASSWD_CANT_CHANGE              = 0x0000040,
+        ENCRYPTED_TEXT_PWD_ALLOWED      = 0x0000080,
+        TEMP_DUPLICATE_ACCOUNT          = 0x0000100,
+        NORMAL_ACCOUNT                  = 0x0000200,
+        INTERDOMAIN_TRUST_ACCOUNT       = 0x0000800,
+        WORKSTATION_TRUST_ACCOUNT       = 0x0001000,
+        SERVER_TRUST_ACCOUNT            = 0x0002000,
+        DONT_EXPIRE_PASSWORD            = 0x0010000,
+        MNS_LOGON_ACCOUNT               = 0x0020000,
+        SMARTCARD_REQUIRED              = 0x0040000,
+        TRUSTED_FOR_DELEGATION          = 0x0080000,
+        NOT_DELEGATED                   = 0x0100000,
+        USE_DES_KEY_ONLY                = 0x0200000,
+        DONT_REQ_PREAUTH                = 0x0400000,
+        PASSWORD_EXPIRED                = 0x0800000,
+        TRUSTED_TO_AUTH_FOR_DELEGATION  = 0x1000000,
+        PARTIAL_SECRETS_ACCOUNT         = 0x4000000,
+    }
+"@
