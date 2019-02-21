937b13e0e82e34679d6980c02247ee147f8867f9
Sean Quinlan <github@yster.org>
2019-02-21 21:24:56 +0000

    After a bit of testing, updated to use the fastest method to sort an object/hashtable

--- Private/ConvertTo-SortedPSObject.ps1
+++ Private/ConvertTo-SortedPSObject.ps1
@@ -6,6 +6,10 @@ function ConvertTo-SortedPSObject {
         Takes an input object of type HashTable or PSObject, and sorts it aphabetically by Property Name. A PSCustomObject with the sorted properties is returned.
     .EXAMPLE
         $SortedResults = ConvertTo-SortedPSObject -InputObject $ResultsObject
+    .NOTES
+        The method of converting to a hashtable and then using that as the input for a new PSObject was fastest in my testing.
+
+        Faster than using Add-Member, and faster than using the $Object.PSObject.Properties.Add($PSNoteProperty) method I use in other places in this module.
     #>
 
     [CmdletBinding()]
@@ -21,24 +25,21 @@ function ConvertTo-SortedPSObject {
 
     try {
         if ($InputObject -is [HashTable]) {
-            # Sort results and then add to a new hashtable, as PSObject requires a hashtable as Property. GetEnumerator() piped into Sort-Object changes the output to an array.
             Write-Verbose ('{0}|HashTable type' -f $Function_Name)
-            $Input_Object_Sorted = [Ordered]@{}
-            $InputObject.GetEnumerator() | Sort-Object -Property 'Name' | ForEach-Object {
-                $Input_Object_Sorted[$_.Name] = $_.Value
-            }
-            New-Object -TypeName 'System.Management.Automation.PSObject' -Property $Input_Object_Sorted
+            $Input_Object_Properties = $InputObject.GetEnumerator()
         } elseif ($InputObject -is [PSObject]) {
             Write-Verbose ('{0}|PSObject type' -f $Function_Name)
-            $Input_Object_Sorted = New-Object -TypeName 'System.Management.Automation.PSObject'
-            $InputObject.PSObject.Properties | Sort-Object -Property 'Name' | ForEach-Object {
-                $Input_Object_Property = New-Object -TypeName 'System.Management.Automation.PSNoteProperty' -ArgumentList @($_.Name, $_.Value)
-                $Input_Object_Sorted.PSObject.Properties.Add($Input_Object_Property)
-            }
-            $Input_Object_Sorted
+            $Input_Object_Properties = $InputObject.PSObject.Properties
         } else {
             Write-Error ('Unknown input object: {0}' -f $InputObject.GetType()) -ErrorAction 'Stop'
         }
+
+        # Sort results and then add to a new hashtable, as PSObject requires a hashtable as Property. GetEnumerator() piped into Sort-Object changes the output to an array.
+        $Input_Object_Sorted = [Ordered]@{}
+        $Input_Object_Properties | Sort-Object -Property 'Name' | ForEach-Object {
+            $Input_Object_Sorted[$_.Name] = $_.Value
+        }
+        New-Object -TypeName 'System.Management.Automation.PSObject' -Property $Input_Object_Sorted
     } catch {
         $PSCmdlet.ThrowTerminatingError($_)
     }
