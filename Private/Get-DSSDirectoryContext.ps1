f8a5bfce93bb90e25c54fb086f882001790945a0
Sean Quinlan <github@yster.org>
2019-02-24 21:42:56 +0000

    First commit of Get-DSSDirectoryContext

---
+++ Private/Get-DSSDirectoryContext.ps1
@@ -0,0 +1,63 @@
+function Get-DSSDirectoryContext {
+    <#
+    .SYNOPSIS
+        This creates a DirectoryContext object, which can be used in System.DirectoryServices.ActiveDirectory methods.
+    .DESCRIPTION
+        Creates a DirectoryContext object, using the specified Context, Server and/or Credentials.
+    .EXAMPLE
+        $Forest_Context = Get-DSSDirectoryContext -Context Forest -Server root.contoso.com
+        [System.DirectoryServices.ActiveDirectory.Forest]::GetForest($Forest_Context)
+    .NOTES
+        References:
+        https://docs.microsoft.com/en-us/dotnet/api/system.directoryservices.activedirectory.directorycontext
+    #>
+
+    [CmdletBinding()]
+    param(
+        # The context to search - Domain or Forest.
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Domain', 'Forest')]
+        [String]
+        $Context,
+
+        # The server/domain/forest to run the query on.
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Forest', 'Domain')]
+        [String]
+        $Server,
+
+        # The credential to use for access.
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty
+    )
+
+    $Function_Name = (Get-Variable MyInvocation -Scope 0).Value.MyCommand.Name
+    $PSBoundParameters.GetEnumerator() | ForEach-Object { Write-Verbose ('{0}|Arguments: {1} - {2}' -f $Function_Name, $_.Key, ($_.Value -join ' ')) }
+
+    try {
+        $Directory_Context_Arguments = @($Context)
+        if ($PSBoundParameters.ContainsKey('Server')) {
+            Write-Verbose ('{0}|Custom server: {1}' -f $Function_Name, $Server)
+            $Directory_Context_Arguments += $Server
+        }
+        if ($PSBoundParameters.ContainsKey('Credential')) {
+            if ($Credential.GetNetworkCredential().Domain) {
+                $Credential_User = ('{0}\{1}' -f $Credential.GetNetworkCredential().Domain, $Credential.GetNetworkCredential().UserName)
+            } else {
+                $Credential_User = $Credential.GetNetworkCredential().UserName
+            }
+            Write-Verbose ('{0}|Custom credential user: {1}' -f $Function_Name, $Credential_User)
+            $Directory_Context_Arguments += $Credential_User
+            $Directory_Context_Arguments += $Credential.GetNetworkCredential().Password
+        }
+
+        # Return the DirectoryContext object
+        New-Object -TypeName 'System.DirectoryServices.ActiveDirectory.DirectoryContext' -ArgumentList $Directory_Context_Arguments
+    } catch {
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
+}
