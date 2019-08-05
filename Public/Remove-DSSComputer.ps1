572c576e6beccb515c3478a8e27fb438e70a542e
Sean Quinlan <github@yster.org>
2019-08-05 20:03:41 +0100

    First commit of Remove-DSSComputer

---
+++ Public/Remove-DSSComputer.ps1
@@ -0,0 +1,104 @@
+function Remove-DSSComputer {
+    <#
+    .SYNOPSIS
+        Removes a specific computer object from Active Directory.
+    .DESCRIPTION
+        Queries Active Directory for a specific computer object and then deletes it, based on one of the following specified parameters:
+            - DistinguishedName
+            - ObjectSID (SID)
+            - ObjectGUID (GUID)
+            - SAMAccountName
+    .EXAMPLE
+        Remove-DSSComputer -ObjectGUID 'b675195d-e637-447e-ad26-baded4df1ba0'
+
+        Deletes the computer with the above GUID.
+    .NOTES
+        References:
+        https://docs.microsoft.com/en-us/powershell/module/addsadministration/remove-adcomputer
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = 'SAM', SupportsShouldProcess = $true)]
+    param(
+        # The SAMAccountName of the computer.
+        [Parameter(Mandatory = $true, ParameterSetName = 'SAM')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('SAM')]
+        [String]
+        $SAMAccountName,
+
+        # The DistinguishedName of the computer.
+        [Parameter(Mandatory = $true, ParameterSetName = 'DistinguishedName')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('DN')]
+        [String]
+        $DistinguishedName,
+
+        # The ObjectSID of the computer.
+        [Parameter(Mandatory = $true, ParameterSetName = 'SID')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('SID')]
+        [String]
+        $ObjectSID,
+
+        # The ObjectGUID of the computer.
+        [Parameter(Mandatory = $true, ParameterSetName = 'GUID')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('GUID')]
+        [String]
+        $ObjectGUID,
+
+        # The context to search - Domain or Forest.
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('Domain', 'Forest')]
+        [String]
+        $Context = 'Domain',
+
+        # The server to connect to.
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Server,
+
+        # The credential to use for access.
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty
+    )
+
+    $Function_Name = (Get-Variable MyInvocation -Scope 0).Value.MyCommand.Name
+    $PSBoundParameters.GetEnumerator() | ForEach-Object { Write-Verbose ('{0}|Arguments: {1} - {2}' -f $Function_Name, $_.Key, ($_.Value -join ' ')) }
+
+    if ($PSBoundParameters.ContainsKey('SAMAccountName')) {
+        if (-not $SAMAccountName.EndsWith('$')) {
+            $PSBoundParameters['SAMAccountName'] = '{0}$' -f $SAMAccountName
+        }
+    }
+
+    $Set_Parameters = @{
+        'SetType' = 'Remove'
+    }
+
+    # This will add the -Confirm parameter if ConfirmPreference is set high enough.
+    # The Set-DSSRawObject doesn't have a default ConfirmImpact set, so this passes the ConfirmImpact from this function if required.
+    if (-not $PSBoundParameters.ContainsKey('Confirm')) {
+        $ConfirmImpact = 'High'
+        if ([System.Management.Automation.ConfirmImpact]::$ConfirmImpact.Value__ -ge [System.Management.Automation.ConfirmImpact]::$ConfirmPreference.Value__) {
+            Write-Verbose ('{0}|Adding Confirm parameter' -f $Function_Name)
+            $Set_Parameters['Confirm'] = $True
+        }
+    }
+
+    try {
+        Write-Verbose ('{0}|Calling Set-DSSRawObject' -f $Function_Name)
+        Set-DSSRawObject @Set_Parameters @PSBoundParameters
+    } catch {
+        if ($_.FullyQualifiedErrorId -match '^DSS-') {
+            $Terminating_ErrorRecord = New-DefaultErrorRecord -InputObject $_
+            $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
+        } else {
+            throw
+        }
+    }
+}
