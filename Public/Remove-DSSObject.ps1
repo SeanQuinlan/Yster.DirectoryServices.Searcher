350adc80768199eaf666cb656a49574ce512db6a
Sean Quinlan <github@yster.org>
2019-08-20 07:02:40 +0100

    Updated for new format of Set-DSSRawObject

--- Public/Remove-DSSObject.ps1
+++ Public/Remove-DSSObject.ps1
@@ -59,23 +59,64 @@ function Remove-DSSObject {
     $Function_Name = (Get-Variable MyInvocation -Scope 0).Value.MyCommand.Name
     $PSBoundParameters.GetEnumerator() | ForEach-Object { Write-Verbose ('{0}|Arguments: {1} - {2}' -f $Function_Name, $_.Key, ($_.Value -join ' ')) }
 
-    $Set_Parameters = @{
-        'SetType' = 'RemoveObject'
-    }
-
-    # This will add the -Confirm parameter if ConfirmPreference is set high enough.
-    # The Set-DSSRawObject doesn't have a default ConfirmImpact set, so this passes the ConfirmImpact from this function if required.
-    if (-not $PSBoundParameters.ContainsKey('Confirm')) {
-        $ConfirmImpact = 'High'
-        if ([System.Management.Automation.ConfirmImpact]::$ConfirmImpact.Value__ -ge [System.Management.Automation.ConfirmImpact]::$ConfirmPreference.Value__) {
-            Write-Verbose ('{0}|Adding Confirm parameter' -f $Function_Name)
-            $Set_Parameters['Confirm'] = $True
-        }
-    }
-
     try {
-        Write-Verbose ('{0}|Calling Set-DSSRawObject' -f $Function_Name)
-        Set-DSSRawObject @Set_Parameters @PSBoundParameters
+        $Common_Search_Parameters = @{
+            'Context' = $Context
+        }
+        if ($PSBoundParameters.ContainsKey('Server')) {
+            $Common_Search_Parameters['Server'] = $Server
+        }
+        if ($PSBoundParameters.ContainsKey('Credential')) {
+            $Common_Search_Parameters['Credential'] = $Credential
+        }
+
+        $Confirm_Parameters = @{}
+        # This will add the -Confirm parameter if ConfirmPreference is set high enough.
+        # The Set-DSSRawObject doesn't have a default ConfirmImpact set, so this passes the ConfirmImpact from this function if required.
+        if (-not $PSBoundParameters.ContainsKey('Confirm')) {
+            $ConfirmImpact = 'High'
+            if ([System.Management.Automation.ConfirmImpact]::$ConfirmImpact.Value__ -ge [System.Management.Automation.ConfirmImpact]::$ConfirmPreference.Value__) {
+                Write-Verbose ('{0}|Adding Confirm parameter' -f $Function_Name)
+                $Confirm_Parameters['Confirm'] = $True
+            }
+        }
+        if ($PSBoundParameters.ContainsKey('WhatIf')) {
+            $Confirm_Parameters['WhatIf'] = $WhatIf
+        }
+
+        if ($PSBoundParameters.ContainsKey('DistinguishedName')) {
+            $LDAPFilter = '(distinguishedname={1})' -f $DistinguishedName
+            $Directory_Search_Type = 'DistinguishedName'
+            $Directory_Search_Value = $DistinguishedName
+        } else {
+            $LDAPFilter = '(objectguid={1})' -f $ObjectGUID
+            $Directory_Search_Type = 'ObjectGUID'
+            $Directory_Search_Value = $ObjectGUID
+        }
+        $Directory_Search_Parameters = @{
+            'LDAPFilter'   = $LDAPFilter
+            'OutputFormat' = 'DirectoryEntry'
+        }
+
+        $Object_Directory_Entry = Find-DSSRawObject @Common_Search_Parameters @Directory_Search_Parameters
+        if ($Object_Directory_Entry) {
+            $Set_Parameters = @{
+                'Action' = 'RemoveObject'
+                'Object' = $Object_Directory_Entry
+            }
+            Write-Verbose ('{0}|Calling Set-DSSRawObject' -f $Function_Name)
+            Set-DSSRawObject @$Common_Search_Parameters @Set_Parameters @Confirm_Parameters
+        } else {
+            $Terminating_ErrorRecord_Parameters = @{
+                'Exception'    = 'System.DirectoryServices.ActiveDirectory.ActiveDirectoryObjectNotFoundException'
+                'ID'           = 'DSS-{0}' -f $Function_Name
+                'Category'     = 'ObjectNotFound'
+                'TargetObject' = $Object_Directory_Entry
+                'Message'      = 'Cannot find Object with {0} of "{1}"' -f $Directory_Search_Type, $Directory_Search_Value
+            }
+            $Terminating_ErrorRecord = New-ErrorRecord @Terminating_ErrorRecord_Parameters
+            $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
+        }
     } catch {
         if ($_.FullyQualifiedErrorId -match '^DSS-') {
             $Terminating_ErrorRecord = New-DefaultErrorRecord -InputObject $_
