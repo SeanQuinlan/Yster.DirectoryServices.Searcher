4c35f81b55df4d7f3d97185446e8f2c5446159de
Sean Quinlan <github@yster.org>
2019-08-26 17:14:23 +0100

    Rearranged some of the validation to allow for changing values as well as validating them

--- Private/Confirm-DSSObjectParameters.ps1
+++ Private/Confirm-DSSObjectParameters.ps1
@@ -11,7 +11,6 @@ function Confirm-DSSObjectParameters {
         $Set_Parameters = Confirm-DSSObjectParameters -BoundParameters $PSBoundParameters
 
         Validates the parameters supplied and throws an error if any are found. If all parameters are correct, returns a hashtable in a format suitable to be passed to Set-DSSRawObject.
-    .NOTES
     #>
 
     [CmdletBinding()]
@@ -33,8 +32,8 @@ function Confirm-DSSObjectParameters {
         # Add any other bound parameters, excluding the ones in $All_CommonParameters and in the $Set_Choices above.
         foreach ($Parameter_Key in $BoundParameters.Keys) {
             if (($All_CommonParameters + $Set_Choices) -notcontains $Parameter_Key) {
-                if ($Microsoft_Alias_Properties.Values -contains $Parameter_Key) {
-                    $Parameter_Name = ($Microsoft_Alias_Properties.GetEnumerator() | Where-Object { $_.Value -eq $Parameter_Key }).'Name'
+                if ($Combined_Calculated_Properties.Values -contains $Parameter_Key) {
+                    $Parameter_Name = ($Combined_Calculated_Properties.GetEnumerator() | Where-Object { $_.Value -eq $Parameter_Key }).'Name'
                 } else {
                     $Parameter_Name = $Parameter_Key
                 }
@@ -51,8 +50,8 @@ function Confirm-DSSObjectParameters {
                 if ($Set_Choice -eq 'Clear') {
                     $New_Set_Choice_Values = New-Object -TypeName 'System.Collections.Generic.List[Object]'
                     foreach ($Current_Value in $Set_Choice_Values) {
-                        if ($Microsoft_Alias_Properties.Values -contains $Current_Value) {
-                            $LDAP_Property = ($Microsoft_Alias_Properties.GetEnumerator() | Where-Object { $_.Value -eq $Current_Value }).'Name'
+                        if ($Combined_Calculated_Properties.Values -contains $Current_Value) {
+                            $LDAP_Property = ($Combined_Calculated_Properties.GetEnumerator() | Where-Object { $_.Value -eq $Current_Value }).'Name'
                             $Property_To_Add = $LDAP_Property
                         } else {
                             $Property_To_Add = $Current_Value
@@ -65,8 +64,8 @@ function Confirm-DSSObjectParameters {
                 } else {
                     $New_Set_Choice_Values = @{}
                     foreach ($Current_Value in $Set_Choice_Values.GetEnumerator()) {
-                        if ($Microsoft_Alias_Properties.Values -contains $Current_Value.Name) {
-                            $LDAP_Property = ($Microsoft_Alias_Properties.GetEnumerator() | Where-Object { $_.Value -eq $Current_Value.Name }).'Name'
+                        if ($Combined_Calculated_Properties.Values -contains $Current_Value.Name) {
+                            $LDAP_Property = ($Combined_Calculated_Properties.GetEnumerator() | Where-Object { $_.Value -eq $Current_Value.Name }).'Name'
                             $Property_To_Add = @{
                                 $LDAP_Property = $Current_Value.Value
                             }
@@ -83,8 +82,8 @@ function Confirm-DSSObjectParameters {
                 }
                 if ($Conflicting_Parameter) {
                     # Get the Microsoft Alias property as well (if there is one), to make the error message better.
-                    if ($Microsoft_Alias_Properties.Keys -contains $Conflicting_Parameter) {
-                        $Conflicting_Parameter = ($Conflicting_Parameter, ($Microsoft_Alias_Properties[$Conflicting_Parameter])) -join '/'
+                    if ($Combined_Calculated_Properties.Keys -contains $Conflicting_Parameter) {
+                        $Conflicting_Parameter = ($Conflicting_Parameter, ($Combined_Calculated_Properties[$Conflicting_Parameter])) -join '/'
                     }
                     $Terminating_ErrorRecord_Parameters = @{
                         'Exception'    = 'System.ArgumentException'
@@ -105,25 +104,50 @@ function Confirm-DSSObjectParameters {
         # Perform some additional validation on the supplied values. This needs to be done here in order to validate the values passed in via Add/Replace/Remove hashtables.
         foreach ($Choice in @('Replace', 'Add')) {
             if ($Set_Parameters[$Choice]) {
+                foreach ($Key in @($Set_Parameters[$Choice].Keys)) {
+                    switch -Regex ($Key) {
+                        'homedrive' {
+                            if ($Key -notmatch '^[A-Z]{1}:') {
+                                $Terminating_ErrorRecord_Parameters = @{
+                                    'Exception'    = 'System.ArgumentException'
+                                    'ID'           = 'DSS-{0}' -f $Function_Name
+                                    'Category'     = 'InvalidArgument'
+                                    'TargetObject' = $Key
+                                    'Message'      = 'HomeDrive value must be a single letter followed by a colon. Eg. "H:"'
+                                }
+                                $Terminating_ErrorRecord = New-ErrorRecord @Terminating_ErrorRecord_Parameters
+                                $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
+                            }
+                        }
+                        'accountexpires' {
+                            try {
+                                if ($Set_Parameters[$Choice][$Key].ToString() -ne '0') {
+                                    $Check_Date = Get-Date -Date $Set_Parameters[$Choice][$Key]
+                                    Write-Verbose ('{0}|Reformatting DateTime to FileTime: {1}' -f $Function_Name, $Key)
+                                    $Set_Parameters[$Choice][$Key] = $Check_Date.ToFileTime()
+                                }
+                                # These numbers have to be passed as strings in order to set them.
+                                $Set_Parameters[$Choice][$Key] = $Set_Parameters[$Choice][$Key].ToString()
+                            } catch {
+                                $Terminating_ErrorRecord_Parameters = @{
+                                    'Exception'      = 'System.FormatException'
+                                    'ID'             = 'DSS-{0}' -f $Function_Name
+                                    'Category'       = 'InvalidArgument'
+                                    'TargetObject'   = $Check_Date
+                                    'Message'        = $_.Exception.InnerException.Message
+                                    'InnerException' = $_.Exception
+                                }
+                                $Terminating_ErrorRecord = New-ErrorRecord @Terminating_ErrorRecord_Parameters
+                                $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
+                            }
+                        }
+                    }
+                }
                 $Set_Parameters_To_Validate += $Set_Parameters[$Choice].GetEnumerator()
             }
         }
-        foreach ($Parameter in $Set_Parameters_To_Validate) {
-            if ($Parameter.Name -eq 'HomeDrive') {
-                if ($Parameter.Value -notmatch '^[A-Z]{1}:') {
-                    $Terminating_ErrorRecord_Parameters = @{
-                        'Exception'    = 'System.ArgumentException'
-                        'ID'           = 'DSS-{0}' -f $Function_Name
-                        'Category'     = 'InvalidArgument'
-                        'TargetObject' = $Parameter
-                        'Message'      = 'HomeDrive value must be a single letter followed by a colon. Eg. "H:"'
-                    }
-                    $Terminating_ErrorRecord = New-ErrorRecord @Terminating_ErrorRecord_Parameters
-                    $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
-                }
-            }
-        }
 
+        # Return the $Set_Parameters hashtable.
         $Set_Parameters
     } catch {
         if ($_.FullyQualifiedErrorId -match '^DSS-') {
