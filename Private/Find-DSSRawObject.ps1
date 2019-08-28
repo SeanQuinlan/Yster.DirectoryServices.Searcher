3e20fa634ec935fbf0bdc493d35b8915acec04be
Sean Quinlan <github@yster.org>
2019-08-28 21:39:05 +0100

    Moved Useful_Calculated_Subproperties to Shared_Variables

--- Private/Find-DSSRawObject.ps1
+++ Private/Find-DSSRawObject.ps1
@@ -105,58 +105,6 @@ function Find-DSSRawObject {
         'objectsid'
     )
 
-    # Like $Useful_Calculated_Properties, these are also calculated based on another property, but require some additional calculation on the sub-property as well.
-    $Useful_Calculated_SubProperties = @{
-        # A number of properties returned by the AD Cmdlets are calculated based on flags to one of the UserAccountControl LDAP properties.
-        # The list of flags and their corresponding values are taken from here:
-        # - https://support.microsoft.com/en-us/help/305144/how-to-use-the-useraccountcontrol-flags-to-manipulate-user-account-pro
-        'useraccountcontrol'                 = @{
-            'accountnotdelegated'               = '0x0100000'
-            'allowreversiblepasswordencryption' = '0x0000080'
-            'doesnotrequirepreauth'             = '0x0400000'
-            'enabled'                           = '0x0000002'
-            'homedirrequired'                   = '0x0000008'
-            'mnslogonaccount'                   = '0x0020000'
-            'passwordneverexpires'              = '0x0010000'
-            'passwordnotrequired'               = '0x0000020'
-            'smartcardlogonrequired'            = '0x0040000'
-            'trustedfordelegation'              = '0x0080000'
-            'trustedtoauthfordelegation'        = '0x1000000'
-            'usedeskeyonly'                     = '0x0200000'
-        }
-        'msds-user-account-control-computed' = @{
-            'lockedout'       = '0x0000010'
-            'passwordexpired' = '0x0800000'
-        }
-
-        # Get-ADDomain provides a number of "Container" properties which are calculated from the WellknownObjects or OtherWellknownObjects properties.
-        # - Values taken from https://support.microsoft.com/en-gb/help/324949/redirecting-the-users-and-computers-containers-in-active-directory-dom
-        'wellknownobjects'                   = @{
-            'computerscontainer'                 = 'B:32:AA312825768811D1ADED00C04FD8D5CD:'
-            'deletedobjectscontainer'            = 'B:32:18E2EA80684F11D2B9AA00C04F79F805:'
-            'domaincontrollerscontainer'         = 'B:32:A361B2FFFFD211D1AA4B00C04FD7D83A:'
-            'foreignsecurityprincipalscontainer' = 'B:32:22B70C67D56E4EFB91E9300FCA3DC1AA:'
-            'infrastructurecontainer'            = 'B:32:2FBAC1870ADE11D297C400C04FD8D5CD:'
-            'lostandfoundcontainer'              = 'B:32:AB8153B7768811D1ADED00C04FD8D5CD:'
-            'microsoftprogramdatacontainer'      = 'B:32:F4BE92A4C777485E878E9421D53087DB:'
-            'programdatacontainer'               = 'B:32:09460C08AE1E4A4EA0F64AEE7DAA1E5A:'
-            'quotascontainer'                    = 'B:32:6227F0AF1FC2410D8E3BB10615BB5B0F:'
-            'systemscontainer'                   = 'B:32:AB1D30F3768811D1ADED00C04FD8D5CD:'
-            'userscontainer'                     = 'B:32:A9D1CA15768811D1ADED00C04FD8D5CD:'
-        }
-        'otherwellknownobjects'              = @{
-            'keyscontainer'                   = 'B:32:683A24E2E8164BD3AF86AC3C2CF3F981:'
-            'managedserviceaccountscontainer' = 'B:32:1EB93889E40C45DF9F0C64D23BBB6237:'
-        }
-
-        # These are calculated from the 'pwdproperties' property.
-        # - Values taken from: https://docs.microsoft.com/en-us/windows/desktop/adschema/a-pwdproperties
-        'pwdproperties'                      = @{
-            'complexityenabled'           = '0x01'
-            'reversibleencryptionenabled' = '0x10'
-        }
-    }
-
     # Some additional flags to the 'msds-supportedencryptiontypes' property which don't form part of the ADKerberosEncryptionTypes Enum.
     # - Taken from https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-kile/6cfc7b50-11ed-4b4d-846d-6f08f0812919
     $Additional_Encryption_Types = @{
