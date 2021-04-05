4cb7f06f4ae46735b84e090f4111496bc6feaf5e
Sean Quinlan <github@yster.org>
2021-04-05 21:13:41 +0100

    Initial commit of Clear-DSSAccountExpiration - just a simple wrapper script

---
+++ Public/Clear-DSSAccountExpiration.ps1
@@ -0,0 +1,109 @@
+function Clear-DSSAccountExpiration {
+    <#
+    .SYNOPSIS
+        Clears the Account Expiration field for an Active Directory account.
+    .DESCRIPTION
+        Clears the Account Expiration attribute on a computer, user or service account in Active Directory.
+    .EXAMPLE
+        Clear-DSSAccountExpiration -SAMAccountName 'Guest'
+
+        Clear the account expiration date on the "Guest" account.
+    .NOTES
+        References:
+        https://docs.microsoft.com/en-us/powershell/module/addsadministration/clear-adaccountexpiration
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = 'SAM', SupportsShouldProcess = $true)]
+    param(
+        # The directory context to search - Domain or Forest. By default this will search within the domain only.
+        # If you want to search the entire directory, specify "Forest" for this parameter and the search will be performed on a Global Catalog server, targetting the entire forest.
+        # An example of using this property is:
+        #
+        # -Context 'Forest'
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('Domain', 'Forest')]
+        [String]
+        $Context = 'Domain',
+
+        # The credential to use for access to perform the required action.
+        # This credential can be provided in the form of a username, DOMAIN\username or as a PowerShell credential object.
+        # In the case of a username or DOMAIN\username, you will be prompted to supply the password.
+        # Some examples of using this property are:
+        #
+        # -Credential jsmith
+        # -Credential 'CONTOSO\jsmith'
+        #
+        # $Creds = Get-Credential
+        # -Credential $Creds
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty,
+
+        # The DistinguishedName of the account.
+        [Parameter(Mandatory = $true, ParameterSetName = 'DistinguishedName')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('DN')]
+        [String]
+        $DistinguishedName,
+
+        # The ObjectGUID of the account.
+        [Parameter(Mandatory = $true, ParameterSetName = 'GUID')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('GUID')]
+        [String]
+        $ObjectGUID,
+
+        # The ObjectSID of the account.
+        [Parameter(Mandatory = $true, ParameterSetName = 'SID')]
+        [ValidateNotNullOrEmpty()]
+        [Alias('SID')]
+        [String]
+        $ObjectSID,
+
+        # The SAMAccountName of the account.
+        [Parameter(Mandatory = $true, ParameterSetName = 'SAM')]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript(
+            {
+                if ($_ -match '[\*\?]') {
+                    throw [System.Management.Automation.ValidationMetadataException] 'Cannot contain wildcards'
+                } else {
+                    $true
+                }
+            }
+        )]
+        [Alias('SAM')]
+        [String]
+        $SAMAccountName,
+
+        # The server or domain to connect to.
+        # See below for some examples:
+        #
+        # -Server DC01
+        # -Server 'dc01.contoso.com'
+        # -Server CONTOSO
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Server
+    )
+
+    $Function_Name = (Get-Variable MyInvocation -Scope 0).Value.MyCommand.Name
+    $PSBoundParameters.GetEnumerator() | ForEach-Object { Write-Verbose ('{0}|Arguments: {1} - {2}' -f $Function_Name, $_.Key, ($_.Value -join ' ')) }
+
+    try {
+        Write-Verbose ('{0}|Calling Set-DSSObjectWrapper' -f $Function_Name)
+        $PSBoundParameters['AccountExpirationDate'] = $null
+        Set-DSSObjectWrapper -ObjectType 'Account' -BoundParameters $PSBoundParameters
+
+    } catch {
+        if ($_.FullyQualifiedErrorId -match '^DSS-') {
+            $Terminating_ErrorRecord = New-DefaultErrorRecord -InputObject $_
+            $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
+        } else {
+            throw
+        }
+    }
+}
